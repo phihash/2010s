d2a533a0d619c522774c4d492f50cd829c8cbb35
phihash <insharp0220@gmail.com>
2022-04-25 13:20:11 +0900

    イベントリスナーで入力されるたびに値を評価し、数値でなかったら注意書きを表示するようにした

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const selectYear = document.getElementById("select-year");
+const selectYearInput = document.getElementById("select-year");
+const attentionText = document.getElementById("attention-text");
 
 
-selectYear.addEventListener("input",() => {
-  console.log(selectYear.value);
-  console.log(typeof selectYear.value);
+selectYearInput.addEventListener("input",() => {
+  const selectYear = selectYearInput.value;
+
+  if(isNaN(Number(selectYear))){//入力された値が数値以外であった場合
+    attentionText.innerHTML = "数値以外のものが含まれています"
+    return ;
+  }
+
+  attentionText.innerHTML = null
+
+  console.log(Number(selectYear));
+  console.log(typeof Number(selectYear));
 })
 
