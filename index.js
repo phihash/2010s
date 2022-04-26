d411dd45e7fa8f3fbe58d8c23a122e226930717f
phihash <insharp0220@gmail.com>
2022-04-26 14:45:06 +0900

    wip

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 'use strict';
 const selectYearInput = document.getElementById("select-year");
 const attentionText = document.getElementById("attention-text");
-const wordDivided = document.getElementById("word-area");
-const personDivided = document.getElementById("person-area");
-const eventDivided = document.getElementById("event-area");
 const resultDevided = document.getElementById("result-area");
 
 selectYearInput.addEventListener("input",() => {
@@ -19,49 +16,58 @@ selectYearInput.addEventListener("input",() => {
 
    for (const elem of descriptionOfYear) {
       if(selectYear in elem){ 
-        wordDivided.innerHTML = elem[selectYear].word;
-        personDivided.innerHTML = elem[selectYear].person;
-        eventDivided.innerHTML = elem[selectYear].event;
+        const {foods,words,persons,events} = elem[selectYear]; 
+        resultDevided.append(createDescriptionOfItem(words,"ワード"))
+        resultDevided.append(createDescriptionOfItem(foods,"食べ物"))
+        resultDevided.append(createDescriptionOfItem(persons,"人物"))
+        resultDevided.append(createDescriptionOfItem(events,"出来事"))
         return;
       }
   }
 
-  
 })
+
+/**
+ * ある項目とその項目を説明する要素を生成する
+ * @param {obj} obj
+ * @returns {}
+ */
+function createDescriptionOfItem(obj,category){
+  const Divided = document.createElement("div");
+  const categoryName = document.createElement("h3");
+  categoryName.innerHTML = category;
+  Divided.append(categoryName);
+  for(const prop in obj){
+    const itemName = document.createElement("h4");
+    const textOfItem = document.createElement("p");
+    itemName.innerHTML = prop;
+    textOfItem.innerHTML = obj[prop];
+    Divided.append(itemName,textOfItem);
+  }
+  return Divided;
+}
+
 
 // データ
 const descriptionOfYear = [
   {
     "2021":{
-      "food":[
-      {
-        "タピオカ":"ミルクティーと一緒に流行しました"
-      },
-      {
+      "foods":{
+        "タピオカ":"ミルクティーと一緒に流行しました<br>",
         "ピスタチオ":"ピスタチオを利用した商品と一緒に流行しました。"
-      }
-      ],
-      "word":[
-        {
-          "タピる":"タピオカの流行を受け、タピオカを飲みに行くことをタピると言いました"
-        },
-        {
+      },
+      "words":{
+          "タピる":"タピオカの流行を受け、タピオカを飲みに行くことをタピると言いました。<br>",
           "あざまる水産":"ありがとうの意味を言います。"
-        }
-      ],
-      "person":[
+      },
+      "persons":
         {
-          "BE-FIRST":"オーディンから選出されました"
-        },
-        {
+          "BE-FIRST":"オーディンから選出されました。<br>",
           "INI":"韓国の某有名オーディション番組から11人がデビューしました"
-        }
-      ],
-      "event":[
-        {
+        },
+      "events":{
           "東京オリンピック":"コロナの影響で1年延期されましたが無事開催されました"
-        }
-      ]
+      }
     }
   },
   {
